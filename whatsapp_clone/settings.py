98550656eb12a4b2f38544b684ef7a9955e4769f
nareshb07 <k.nareshb0077@gmail.com>
2023-10-30 12:50:17 +0530

    main

--- whatsapp_clone/settings.py
+++ whatsapp_clone/settings.py
@@ -168,7 +168,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    
     # 'content/static',
 ]
 # STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
